420d427cb30acb4ba71e8a0d7461ac086ca52b8d
sehaj anand <sehajanand.321@gmail.com>
2022-01-02 13:43:48 +0530

    third commit

---
+++ src/Components/Loadingspinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./Loadingspinner.css";
+
+export default function Loadingspinner() {
+    return (
+        <div class="image">
+        <img src="./Assets/logo3.png" alt="logo_not_loaded"/>
+        <div class="spin spinner"></div>
+      </div>
+    )
+}
